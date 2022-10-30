7558ef7c836b359791e94172b3cc82b4e508b5d5
gpuccio87 <gpuccio87@gmail.com>
2022-10-30 21:50:29 +0100

    Update contact_form.php

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -15,13 +15,18 @@ if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'
     $secret = '6LdqmCAUAAAAANONcPUkgVpTSGGqm60cabVMVaON';
     //get verify response data
 
-    $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+    //$c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+	$c = curl_init('https://docs.google.com/forms/d/e/1FAIpQLSe3c76-V1S75w-e_UYHPc_WFaW9t4ZH7xrGss4p2dUIhJj06Q/formResponse?&entry.1356265276='.$fields[0].'&entry.1282836463=dfgdfg&entry.528392803=dfgdfg&entry.1308078063=dfgdfg']);
     curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
     $verifyResponse = curl_exec($c);
 
     $responseData = json_decode($verifyResponse);
     if($responseData->success):
-
+	
+		$c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+		$verifyResponse = curl_exec($c);
+		
         try
         {
             $emailText = nl2br("You have new message from Contact Form\n");
